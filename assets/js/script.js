85429fcc7995702de241fede9f576f2d86f57ebe
Amelie Pira <amelie.pira@gmail.com>
2022-05-06 22:47:29 +0100

    styled page with css and bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,1 @@
-const weatherIcon = {
-  sunny: `<i class="fa-solid fa-sun"></i>`,
-  partiallyOvercast: `<i class="fa-solid fa-cloud-sun"></i>`,
-  overcast: `<i class="fa-solid fa-cloud"></i>`,
-  rainy: `<i class="fa-solid fa-cloud-showers-heavy"></i>`,
-};
+
