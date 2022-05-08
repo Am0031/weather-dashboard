07a3c9a9698dbaceb36b95e1c99fe4fe847abaa4
Amelie Pira <amelie.pira@gmail.com>
2022-05-08 17:25:05 +0100

    added fake data in variables for future simulations

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,386 @@
 
 const appid = "e5cd5aafaf451f96b10b7a70a90ea75b";
 const currentBaseUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
+  "https://api.openweathermap.org/data/2.5/weather?units={imperial}&q={city name}&appid={API key}";
 const forecastBaseUrl =
-  "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}";
+  "https://api.openweathermap.org/data/2.5/onecall?units={imperial}&lat={lat}&lon={lon}&exclude={part}&appid={API key}";
 let searchList = [];
+//temporary variables and arrays used during development
+const tempSearchList = ["london", "madrid", "new york", "paris"];
+const tempEmptyList = [];
+const tempCurrentWeatherFromApi = {
+  coord: {
+    lon: -0.13,
+    lat: 51.51,
+  },
+  weather: [
+    {
+      id: 300,
+      main: "Drizzle",
+      description: "light intensity drizzle",
+      icon: "09d",
+    },
+  ],
+  base: "stations",
+  main: {
+    temp: 44.906,
+    pressure: 1012,
+    humidity: 81,
+    temp_min: 42.8,
+    temp_max: 46.4,
+  },
+  visibility: 10000,
+  wind: {
+    speed: 4.1,
+    deg: 80,
+  },
+  clouds: {
+    all: 90,
+  },
+  dt: 1652000400,
+  sys: {
+    type: 1,
+    id: 5091,
+    message: 0.0103,
+    country: "GB",
+    sunrise: 1485762037,
+    sunset: 1485794875,
+  },
+  id: 2643743,
+  name: "London",
+  cod: 200,
+};
+
+const tempForecastFromApi = {
+  lat: 33.44,
+  lon: -94.04,
+  timezone: "America/Chicago",
+  timezone_offset: -21600,
+  daily: [
+    {
+      dt: 1652000400,
+      sunrise: 1618282134,
+      sunset: 1618333901,
+      moonrise: 1618284960,
+      moonset: 1618339740,
+      moon_phase: 0.04,
+      temp: {
+        day: 43.952,
+        min: 35.492,
+        max: 51.656,
+        night: 35.492,
+        eve: 42.908,
+        morn: 41.612
+      },
+      feels_like: {
+        day: 39.938,
+        night: 37.616,
+        eve: 38.012,
+        morn: 37.616
+      },
+      pressure: 1020,
+      humidity: 81,
+      dew_point: 276.77,
+      wind_speed: 3.06,
+      wind_deg: 294,
+      weather: [
+        {
+          id: 500,
+          main: "Clouds",
+          description: "few clouds",
+          icon: "02d"
+        }
+      ],
+      clouds: 56,
+      pop: 0.2,
+      rain: 0.62,
+      uvi: 1.93
+    },
+    {
+        dt: 1652086800,
+        sunrise: 1618282134,
+      sunset: 1618333901,
+      moonrise: 1618284960,
+      moonset: 1618339740,
+      moon_phase: 0.04,
+      temp: {
+        day: 43.952,
+        min: 35.492,
+        max: 51.656,
+        night: 35.492,
+        eve: 42.908,
+        morn: 41.612
+      },
+      feels_like: {
+        day: 39.938,
+        night: 37.616,
+        eve: 38.012,
+        morn: 37.616
+      },
+      pressure: 1020,
+      humidity: 81,
+      dew_point: 276.77,
+      wind_speed: 3.06,
+      wind_deg: 294,
+      weather: [
+        {
+          id: 500,
+          main: "Clear",
+          description: "clear sky",
+          icon: "01d"
+        }
+      ],
+      clouds: 56,
+      pop: 0.2,
+      rain: 0.62,
+      uvi: 1.93
+    },
+    {
+        dt: 1652173200,
+        sunrise: 1618282134,
+      sunset: 1618333901,
+      moonrise: 1618284960,
+      moonset: 1618339740,
+      moon_phase: 0.04,
+      temp: {
+        day: 43.952,
+        min: 35.492,
+        max: 51.656,
+        night: 35.492,
+        eve: 42.908,
+        morn: 41.612
+      },
+      feels_like: {
+        day: 39.938,
+        night: 37.616,
+        eve: 38.012,
+        morn: 37.616
+      },
+      pressure: 1020,
+      humidity: 81,
+      dew_point: 276.77,
+      wind_speed: 3.06,
+      wind_deg: 294,
+      weather: [
+        {
+          id: 500,
+          main: "Drizzle",
+          description: "light intensity drizzle",
+          icon: "09d"
+        }
+      ],
+      clouds: 56,
+      pop: 0.2,
+      rain: 0.62,
+      uvi: 1.93
+    },
+    {
+        dt: 1652259600,
+        sunrise: 1618282134,
+      sunset: 1618333901,
+      moonrise: 1618284960,
+      moonset: 1618339740,
+      moon_phase: 0.04,
+      temp: {
+        day: 43.952,
+        min: 35.492,
+        max: 51.656,
+        night: 35.492,
+        eve: 42.908,
+        morn: 41.612
+      },
+      feels_like: {
+        day: 39.938,
+        night: 37.616,
+        eve: 38.012,
+        morn: 37.616
+      },
+      pressure: 1020,
+      humidity: 81,
+      dew_point: 276.77,
+      wind_speed: 3.06,
+      wind_deg: 294,
+      weather: [
+        {
+          id: 500,
+          main: "Clouds",
+          description: "scattered clouds",
+          icon: "03n"
+        }
+      ],
+      clouds: 56,
+      pop: 0.2,
+      rain: 0.62,
+      uvi: 1.93
+    },
+    {
+        dt: 1652346000,
+        sunrise: 1618282134,
+      sunset: 1618333901,
+      moonrise: 1618284960,
+      moonset: 1618339740,
+      moon_phase: 0.04,
+      temp: {
+        day: 43.952,
+        min: 35.492,
+        max: 51.656,
+        night: 35.492,
+        eve: 42.908,
+        morn: 41.612
+      },
+      feels_like: {
+        day: 39.938,
+        night: 37.616,
+        eve: 38.012,
+        morn: 37.616
+      },
+      pressure: 1020,
+      humidity: 81,
+      dew_point: 276.77,
+      wind_speed: 3.06,
+      wind_deg: 294,
+      weather: [
+        {
+          id: 500,
+          main: "Rain",
+          description: "light rain",
+          icon: "02d"
+        }
+      ],
+      clouds: 56,
+      pop: 0.2,
+      rain: 0.62,
+      uvi: 1.93
+    },
+    {
+        dt: 1652432400,
+        sunrise: 1618282134,
+      sunset: 1618333901,
+      moonrise: 1618284960,
+      moonset: 1618339740,
+      moon_phase: 0.04,
+      temp: {
+        day: 43.952,
+        min: 35.492,
+        max: 51.656,
+        night: 35.492,
+        eve: 42.908,
+        morn: 41.612
+      },
+      feels_like: {
+        day: 39.938,
+        night: 37.616,
+        eve: 38.012,
+        morn: 37.616
+      },
+      pressure: 1020,
+      humidity: 81,
+      dew_point: 276.77,
+      wind_speed: 3.06,
+      wind_deg: 294,
+      weather: [
+        {
+          id: 500,
+          main: "Rain",
+          description: "light rain",
+          icon: "10d"
+        }
+      ],
+      clouds: 56,
+      pop: 0.2,
+      rain: 0.62,
+      uvi: 1.93
+    },
+    {
+        dt: 1652518800,
+        sunrise: 1618282134,
+      sunset: 1618333901,
+      moonrise: 1618284960,
+      moonset: 1618339740,
+      moon_phase: 0.04,
+      temp: {
+        day: 43.952,
+        min: 35.492,
+        max: 51.656,
+        night: 35.492,
+        eve: 42.908,
+        morn: 41.612
+      },
+      feels_like: {
+        day: 39.938,
+        night: 37.616,
+        eve: 38.012,
+        morn: 37.616
+      },
+      pressure: 1020,
+      humidity: 81,
+      dew_point: 276.77,
+      wind_speed: 3.06,
+      wind_deg: 294,
+      weather: [
+        {
+          id: 500,
+          main: "Rain",
+          description: "light rain",
+          icon: "10d"
+        }
+      ],
+      clouds: 56,
+      pop: 0.2,
+      rain: 0.62,
+      uvi: 1.93
+    },
+    {
+        dt: 1652605200,
+        sunrise: 1618282134,
+      sunset: 1618333901,
+      moonrise: 1618284960,
+      moonset: 1618339740,
+      moon_phase: 0.04,
+      temp: {
+        day: 43.952,
+        min: 35.492,
+        max: 51.656,
+        night: 35.492,
+        eve: 42.908,
+        morn: 41.612
+      },
+      feels_like: {
+        day: 39.938,
+        night: 37.616,
+        eve: 38.012,
+        morn: 37.616
+      },
+      pressure: 1020,
+      humidity: 81,
+      dew_point: 276.77,
+      wind_speed: 3.06,
+      wind_deg: 294,
+      weather: [
+        {
+          id: 500,
+          main: "Rain",
+          description: "light rain",
+          icon: "10d"
+        }
+      ],
+      clouds: 56,
+      pop: 0.2,
+      rain: 0.62,
+      uvi: 1.93
+    }
+    ]
+    alerts: [
+        {
+          "sender_name": "NWS Tulsa",
+          "event": "Heat Advisory",
+          "start": 1597341600,
+          "end": 1597366800,
+          "description": "...HEAT ADVISORY REMAINS IN EFFECT FROM 1 PM THIS AFTERNOON TO\n8 PM CDT THIS EVENING...\n* WHAT...Heat index values of 105 to 109 degrees expected.\n* WHERE...Creek, Okfuskee, Okmulgee, McIntosh, Pittsburg,\nLatimer, Pushmataha, and Choctaw Counties.\n* WHEN...From 1 PM to 8 PM CDT Thursday.\n* IMPACTS...The combination of hot temperatures and high\nhumidity will combine to create a dangerous situation in which\nheat illnesses are possible.",
+          "tags": [
+            "Extreme temperature value"
+            ]}]
+};
 
 //UTILITY FUNCTIONS
 
